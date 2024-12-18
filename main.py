7628623e5bd40c8e7d4b5d1a7ef3575b5ac918a5
TapTapDev <shaxboznoutbook@gmail.com>
2024-12-18 22:57:36 +0500

    HelloWord

---
+++ main.py
@@ -0,0 +1,12 @@
+print('Hello World')
+
+
+"""
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/TapTapDev/HelloWord.git
+git push -u origin main
+
+"""
